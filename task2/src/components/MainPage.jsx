5a26580f0d68d98587402900760f3522ffaf6629
Petar Pejovic <petar.pejovic@datacomplete.net>
2021-12-10 17:22:35 +0100

    kostur

---
+++ task2/src/components/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "./About";
+import Images from "./Images";
+import Form from "./Form";
+
+const MainPage = () => {
+  return (
+    <div>
+      <About />
+      <Images />
+      <Form />
+    </div>
+  );
+};
+
+export default MainPage;
